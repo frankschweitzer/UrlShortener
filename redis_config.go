54225eb5e01051ac2d56d4a480c24599633defaa
frankschweitzer <schwfj20@wfu.edu>
2025-07-17 20:15:28 -0400

    initial commit

---
+++ redis_config.go
@@ -0,0 +1,13 @@
+package main
+
+import "github.com/go-redis/redis/v8"
+
+func redis_client() *redis.Client {
+	client := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
+	})
+
+	return client
+}
